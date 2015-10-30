e8f66d3e90f9810999b71804f9e4c87ccafcaebf
nerik <erik.escoffier@gmail.com>
2015-10-30 12:38:36 +0100

    date filter

--- dist/assets/app.js
+++ dist/assets/app.js
@@ -7,10 +7,31 @@
     var regionsGeoJson;
     var regionsShapes;
 
-    var map, popupTpl, modalTpl, regionsListContainer, regionListItemTpl, datePickerItemTpl;
+    var map, popupTpl, modalTpl, regionsListContainer, regionListItemTpl, datePickerItemTpl, datePickerStatusTpl, datePickerContainers;
     var markerClusters;
 
     var features = [];
+
+    var filterFunctions = {
+      checkboxes: function(filter, feature) {
+        var featureFiltered = feature.properties[filter.field];
+        var featureFilters = filter.values;
+        // ignore filter if field is empty     filter passes when field matches filter
+        return !featureFiltered || featureFiltered.length === 0 || _.intersection(featureFiltered, featureFilters).length
+      },
+      date: function(filter, feature) {
+        console.log(filter)
+        var featureFilteredStart = feature.properties[filter.field_start];
+        var featureFilteredEnd = feature.properties[filter.field_end];
+        var featureFiltersStart = filter.start;
+        var featureFiltersEnd = filter.end;
+
+        var isAfterStart = !featureFilteredStart || moment(featureFilteredStart, filter.format).isAfter( moment(featureFiltersStart, filter.format) );
+        var isBeforeEnd = !featureFilteredEnd || moment(featureFilteredEnd, filter.format).isBefore( moment(featureFiltersEnd, filter.format) );
+
+        return isAfterStart && isBeforeEnd;
+      },
+    }
 
     function buildFeatures(data) {
         L.geoJson(data, {
@@ -24,7 +45,6 @@
             }
         });
 
-        // if (markerClusters) map.removeLayer(markerClusters);
         markerClusters = new L.MarkerClusterGroup({
             showCoverageOnHover: false,
             maxClusterRadius: 40,
@@ -49,22 +69,36 @@
       features.forEach(function(feature) {
         // flag feature for show/hide in later renderMarkers
         feature.show = filters.every(function(filter) {
-          var featureFiltered = feature.properties[filter.field];
-          var featureFilters = filter.values;
-          // ignore filter if field is empty     filter passes when field matches filter
-          return featureFiltered.length === 0 || _.intersection(featureFiltered, featureFilters).length
+          return filterFunctions[filter.type](filter, feature);
         })
       })
       renderMarkers();
     }
 
-    function updateFilters() {
+    function updateFilters(newDate) {
         function updateCheckboxList(name) {
             var values = _.map( $('#' + name + ' input'), function(el) {return el.checked; });
             $('[data-countof=' + name + ']').text( _.contains(values, false) ? _.compact(values).length + '/' + values.length : 'toutes' );
         }
         updateCheckboxList('actionType');
         updateCheckboxList('population');
+
+        if (newDate) {
+          newDate.parents('.dropdown-menu').data('date', newDate.data().date);
+          newDate.parents('.dropdown-menu').data('dateFormatted', newDate.html());
+        }
+
+        $('.js-filter-dates-start-month .js-lbl').html( datePickerContainers.startMonth.data('dateFormatted') )
+        $('.js-filter-dates-start-year .js-lbl').html( datePickerContainers.startYear.data('dateFormatted') )
+        $('.js-filter-dates-end-month .js-lbl').html( datePickerContainers.endMonth.data('dateFormatted') )
+        $('.js-filter-dates-end-year .js-lbl').html( datePickerContainers.endYear.data('dateFormatted') )
+
+        $('.js-filter-dates-status').html(datePickerStatusTpl({
+          startMonth: moment.monthsShort()[datePickerContainers.startMonth.data('date')],
+          endMonth: moment.monthsShort()[datePickerContainers.endMonth.data('date')],
+          startYear: datePickerContainers.startYear.data('date'),
+          endYear: datePickerContainers.endYear.data('date')
+        }));
     }
 
     function getFilters() {
@@ -85,6 +119,23 @@
           })
           filters.push(filter);
         });
+
+        var dateFormat = $('.js-filter-dates').data().filterDatesFormat;
+        var dateFilter = {
+          type: 'date',
+          field_start: $('.js-filter-dates').data().filterDatesFieldStart,
+          field_end: $('.js-filter-dates').data().filterDatesFieldEnd,
+          format: dateFormat,
+          start: moment({
+            month:  datePickerContainers.startMonth.data('date'),
+            year:  datePickerContainers.startYear.data('date')
+          }).format(dateFormat),
+          end: moment({
+            month:  datePickerContainers.endMonth.data('date'),
+            year:  datePickerContainers.endYear.data('date')
+          }).format(dateFormat)
+        }
+        filters.push(dateFilter);
 
         return filters;
     }
@@ -113,31 +164,42 @@
     }
 
     function initDatePicker() {
+
+      datePickerContainers = {
+        startMonth: $('.js-filter-dates-start-month .dropdown-menu'),
+        startYear: $('.js-filter-dates-start-year .dropdown-menu'),
+        endMonth: $('.js-filter-dates-end-month .dropdown-menu'),
+        endYear: $('.js-filter-dates-end-year .dropdown-menu')
+      }
+
       var initialDate = moment('2010-01-01');
       var today = moment();
-      var current = initialDate;
-
-      var startMonthContainer = $('.js-filter-dates-start-month .dropdown-menu');
-      var startYearContainer = $('.js-filter-dates-start-year .dropdown-menu');
-      var endMonthContainer = $('.js-filter-dates-end-month .dropdown-menu');
-      var endYearContainer = $('.js-filter-dates-end-year .dropdown-menu');
+      var current = initialDate.clone();
 
       moment.months().forEach(function(month, index) {
-        var html = datePickerItemTpl({
+        var d = {
           dateFormatted: month,
           date: index
-        });
-        startMonthContainer.append(html);
-        endMonthContainer.append(html);
+        };
+        datePickerContainers.startMonth.append(datePickerItemTpl(d));
+        datePickerContainers.endMonth.append(datePickerItemTpl(d));
+
+        if (index === initialDate.month()) datePickerContainers.startMonth.data(d);
+        if (index === today.month()) datePickerContainers.endMonth.data(d);
       });
 
+
       while(current.isBefore(today)) {
-        var html = datePickerItemTpl({
-          dateFormatted: current.format('YYYY'),
-          date: current.format('YYYY')
-        });
-        startYearContainer.append(html)
-        endYearContainer.append(html)
+        var d = {
+          dateFormatted: current.year(),
+          date: current.year()
+        };
+        datePickerContainers.startYear.append(datePickerItemTpl(d));
+        datePickerContainers.endYear.append(datePickerItemTpl(d));
+
+        if (current.year() === initialDate.year()) datePickerContainers.startYear.data(d);
+        if (current.year() === today.year()) datePickerContainers.endYear.data(d);
+
         current.add(1, 'years');
       }
     }
@@ -167,6 +229,10 @@
         regionsListContainer = $('.js-regions');
         regionListItemTpl = _.template('<li><a href="#" data-latlon="<%= center %>"><%= name %></a></li>');
         datePickerItemTpl = _.template('<li><a href="#" data-date="<%= date %>"><%= dateFormatted %></a></li>');
+        datePickerStatusTpl = _.template('<%= startMonth %> <%= startYear %> → <%= endMonth %> <%= endYear %>');
+
+        moment.locale(window.appConfig.locale);
+        if ($('.js-filter-dates').length) initDatePicker();
 
         // initialize filters count without actually filtering (geoJSON no there yet)
         updateFilters();
@@ -174,26 +240,31 @@
 
         //move that to a sass loop later :)
         _.each($('#actionType .checkboxList input'), function(el, index) {
-            $('<span class="icon"></span>').insertBefore(el).css('background-position', '-' + index * 30 + 'px' + ' 0');
+          $('<span class="icon"></span>').insertBefore(el).css('background-position', '-' + index * 30 + 'px' + ' 0');
         });
 
         _.each($('#population .checkboxList input'), function(el, index) {
-            $('<span class="icon"></span>').insertBefore(el).css('background-position', '-' + index * 30 + 'px' + ' 0');
+          $('<span class="icon"></span>').insertBefore(el).css('background-position', '-' + index * 30 + 'px' + ' 0');
         });
 
 
         $('.js-showfilters').on('click', function () {
-            $('.js-filters').addClass('opened');
+          $('.js-filters').addClass('opened');
         });
 
         $('.js-closeFilters').on('click', function() {
-            $('.js-filters').removeClass('opened');
+          $('.js-filters').removeClass('opened');
         });
 
         $('.js-filter-checkboxes input').on('change', function () {
-            updateFilters();
-            filterMarkers(getFilters());
+          updateFilters();
+          filterMarkers(getFilters());
         });
+
+        $('.js-filter-dates li').on('click', function(e) {
+          updateFilters($(e.target));
+          filterMarkers(getFilters());
+        })
 
         $.ajax(window.appConfig.testDataPath).done( buildFeatures );
         // $.ajax('http://195.154.35.191:8000/geoactions/').done( buildMarkers );
@@ -214,8 +285,6 @@
             initRegionsListEvents();
         } );
 
-        moment.locale(window.appConfig.locale);
-        if ($('.js-filter-dates').length) initDatePicker();
 
         $('#map').on('click', '.js-openContentModal', function(e) {
             e.preventDefault();
